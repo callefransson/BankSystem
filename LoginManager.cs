b2134ddf416193576850bae69e31ccc938422058
calle fransson <calle-fransson@hotmail.com>
2023-11-14 11:43:09 +0100

    added LoginManager class

---
+++ LoginManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    public class LoginManager
+    {
+        private List<Person> _users;
+        public LoginManager()
+        {
+            _users = new List<Person>();
+
+            _users.Add(new Person("admin", "123"));
+            _users.Add(new Person("bankowner", "123"));
+            _users.Add(new Person("user", "123"));
+        }
+
+        public bool Login(string username, string password)
+        {
+            foreach (Person user in _users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
